c81a11b75b88625062c62e340b4a420ab94e73cf
Eric <eb11307@gmail.com>
2015-03-20 15:51:18 -0400

    Added radioDropdown binding to shinyMB.js

--- inst/tests/test.R
+++ inst/tests/test.R
@@ -11,12 +11,14 @@ app <- shinyApp(
               textInput("testTextInput", "Testing"),
               actionDropdown("testDD", "Action", list("Cat", "Dog", NULL, "Rat", "Sheep", "Goat", "Horse and Buggy" = "hnb"),
                              icons = list("github-alt", NULL, NULL, NULL, "child", NULL, NULL)),
-              toggleDropdown("testTogDD", "Checkbox", list("Ant", "Grasshopper", "cricket", "spider"), selected = c("spider", "Ant"))
+              toggleDropdown("testTogDD", "Checkbox", list("Ant", "Grasshopper", "cricket", "spider"), selected = c("spider", "Ant")),
+              toggleDropdown("testTogDD2", "Radio", list("Ant", "Grasshopper", "cricket", "spider"), selected = c("spider", "Ant"), type = "radio")
       )
     ),
   server = 
     function(input, output, session) {
       observeEvent(input$testTogDD, ({print(input$testTogDD)}))
+      observeEvent(input$testTogDD2, ({print(input$testTogDD2)}))
     }
 )
 
--- inst/www/shinyMB.js
+++ inst/www/shinyMB.js
@@ -42,3 +42,43 @@ $.extend(shinyMB.inputBindings.checkboxDropdown, {
   
 })
 Shiny.inputBindings.register(shinyMB.inputBindings.checkboxDropdown);
+
+shinyMB.inputBindings.radioDropdown = new Shiny.InputBinding();
+$.extend(shinyMB.inputBindings.radioDropdown, {
+  find: function(scope) {
+    return $(scope).find(".smb-radio-dropdown");    
+  },
+  getValue: function(el) {
+    return $(el).data("smb-value");
+  },
+  subscribe: function(el, callback) {
+    $(el).find("a.smb-label").on("click", function(ev) {
+      
+      var $tar = $(ev.target);
+      var $sibs = $tar.parent().siblings().find("a.smb-label");
+      var val = $tar.data("smb-value");
+      
+      $sibs.find("i").removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+      $tar.find("i").removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+      $(el).data("smb-value", val)
+      
+      callback();
+      
+    })
+  },
+  initialize: function(el) {
+    
+    var gval = "";
+    
+    $(el).find("a.smb-label").each(function(i, element) {
+      if($(element).find("i").hasClass("fa-dot-circle-o")) {
+        gval = $(element).data("smb-value");
+      }
+    })
+  
+    $(el).data("smb-value", gval);
+  
+  }
+  
+})
+Shiny.inputBindings.register(shinyMB.inputBindings.radioDropdown);
